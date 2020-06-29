0404b173ebd1b117afab490064f3abbf42672325
glenn <glennrudge@outlook.com>
2020-06-29 12:56:30 -0400

    added footer and header partials to main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,32 +12,30 @@
     <!-- Google Fonts Roboto -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <!-- Material Design Bootstrap -->
-    <link rel="stylesheet" href="css/mdb.min.css">
+    <link rel="stylesheet" href="{{ asset('css/mdb.min.css') }}">
     <!-- Your custom styles (optional) -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 
 <!-- Start your project here-->
-<div style="height: 100vh">
-    <div class="flex-center flex-column">
-        <h1 class="text-hide animated fadeIn mb-4" style="background-image: url('https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'); width: 250px; height: 90px;">MDBootstrap</h1>
-        <h5 class="animated fadeIn mb-3">Thank you for using our product. We're glad you're with us.</h5>
-        <p class="animated fadeIn text-muted">MDB Team</p>
-    </div>
-</div>
+@include('layouts.navbar')
+
+@yield('content')
+
+@include('layouts.footer')
 <!-- End your project here-->
 
 <!-- jQuery -->
-<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
 <!-- Bootstrap tooltips -->
-<script type="text/javascript" src="js/popper.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
 <!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
 <!-- MDB core JavaScript -->
-<script type="text/javascript" src="js/mdb.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/mdb.min.js') }}"></script>
 <!-- Your custom scripts (optional) -->
 <script type="text/javascript"></script>
 
