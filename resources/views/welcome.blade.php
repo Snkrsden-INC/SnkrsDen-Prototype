317835d4ad458524f45344deb26d031d9dc9c32a
glenn <glennrudge@outlook.com>
2020-06-30 12:25:11 -0400

    made changes to welcome.blde.php view, created PostsController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,481 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Main Page</h1>
+    <!--Main layout-->
+    <main>
+        <div class="container mt-5 mb-4">
+
+            <!--Section: Posts-->
+            <section>
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(13).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(14).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(15).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(16).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(17).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(19).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+            </section>
+            <!--Section: Posts-->
+
+
+            <!--Section: Pagination-->
+            <section class="d-flex justify-content-center mt-3">
+
+                <style>
+                    .pagination .page-item.active .page-link {
+                        background-color: #1266f1 !important;
+                    }
+                </style>
+
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination pagination-circle">
+                        <li class="page-item disabled">
+                            <a class="page-link" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item active"><a class="page-link">1</a></li>
+                        <li class="page-item"><a class="page-link">2</a></li>
+                        <li class="page-item"><a class="page-link">3</a></li>
+                        <li class="page-item"><a class="page-link">4</a></li>
+                        <li class="page-item"><a class="page-link">5</a></li>
+                        <li class="page-item">
+                            <a class="page-link" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </section>
+            <!--Section: Pagination-->
+
+        </div>
+    </main>  <!--Main layout-->
+    <main>
+        <div class="container mt-5 mb-4">
+
+            <!--Section: Posts-->
+            <section>
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(13).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(14).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(15).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(16).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(17).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6 mb-4">
+
+                        <!-- Card -->
+                        <div class="">
+
+                            <div class="view overlay z-depth-2 rounded">
+                                <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/new/img(19).jpg" alt="Sample">
+                                <a href="#!">
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+
+                            <div class="text-center pt-4">
+
+                                <h5>This is a post title</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam cupiditate veniam voluptatibus
+                                    laudantium cum dolorem illo. Quos architecto deserunt saepe.</p>
+
+                                <button type="button" class="btn btn-primary btn-rounded btn-sm mr-1 mb-2">read more</button>
+
+                            </div>
+
+                        </div>
+                        <!-- Card -->
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+            </section>
+            <!--Section: Posts-->
+
+
+            <!--Section: Pagination-->
+            <section class="d-flex justify-content-center mt-3">
+
+                <style>
+                    .pagination .page-item.active .page-link {
+                        background-color: #1266f1 !important;
+                    }
+                </style>
+
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination pagination-circle">
+                        <li class="page-item disabled">
+                            <a class="page-link" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item active"><a class="page-link">1</a></li>
+                        <li class="page-item"><a class="page-link">2</a></li>
+                        <li class="page-item"><a class="page-link">3</a></li>
+                        <li class="page-item"><a class="page-link">4</a></li>
+                        <li class="page-item"><a class="page-link">5</a></li>
+                        <li class="page-item">
+                            <a class="page-link" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </section>
+            <!--Section: Pagination-->
+
+        </div>
+    </main>
 @endsection
